8ab8e05dc3dc21db498ab0cb321f7ae55adef887
skydzo <283092@supinfo.com>
2019-03-20 22:11:53 +0100

    adding some funcs

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import jsonify
+import subprocess
+
 
 app = Flask(__name__)
 
@@ -6,6 +9,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/api/<direction>/<speed>')
+def move_request(direction,speed):
+    return direction
+
+
+@app.route('/api/camera/<state>')
+def camera_request(state):
+    subprocess.run(["raspi-live", state])
+    return state
 
 
 if __name__ == '__main__':
