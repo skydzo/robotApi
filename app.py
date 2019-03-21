7bb96d474e375ede35b8e0398f75bff3d5e9a9ab
skydzo <283092@supinfo.com>
2019-03-21 10:00:36 +0100

    wsgi

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/api/<direction>/<speed>')
+@app.route('/<direction>/<speed>')
 def move_request(direction,speed):
     robot.move(direction,speed)
     return direction
 
 
-@app.route('/api/camera/<state>')
+@app.route('/camera/<state>')
 def camera_request(state):
     robot.camera(state)
     return state
