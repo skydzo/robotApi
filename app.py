d77f8df9866a077443f6779f52ae10d7e12f9e64
skydzo <283092@supinfo.com>
2019-03-26 23:48:10 +0100

    wsgi

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 from flask import Flask
-import robot
+from gpiozero import Robot
+import os
+# import robot
 
 
 app = Flask(__name__)
+
+
+robot = Robot(left=(8, 7), right=(9, 10))
+
+
+def move(direction, speed):
+    if direction == "forward":
+        robot.forward()
+    if direction == "backward":
+        robot.backward()
+    if direction == "left":
+        robot.left()
+    if direction == "right":
+        robot.right()
+    if direction == "stop":
+        robot.stop()
+
+
+def camera(state):
+    if state == "start":
+        os.system('sudo /bin/sh /var/www/html/robotApi/runCamera.sh pi')
+    if state == "stop":
+        os.system('sudo /bin/sh /var/www/html/robotApi/stopCamera.sh')
 
 
 @app.route('/')
